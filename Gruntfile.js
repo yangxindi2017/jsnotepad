b7e6ab4b83d2cf9fa8188adee338dcf07395c5fd
yangxindi2017 <694076236@qq.com>
2020-04-09 16:39:28 +0800

    travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 /* global module: true */
-module.exports =function(grunt){
-  
+module.exports = function (grunt) {
   grunt.initConfig({
     htmlhint: {
       options: {
@@ -42,7 +41,7 @@ module.exports =function(grunt){
         src: './index.html',
         dest: './dist/index.html'
       }
-    }, 
+    },
     concat: {
       js: {
         src: ['js/*.js', './com/**/*.js'],
@@ -72,21 +71,20 @@ module.exports =function(grunt){
       end: ['dist/bundle.css', 'dist/bundle.js', '.tmp']
     }
   });
-                                      
+
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-eslint');
+
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-usemin');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-  grunt.registerTask('build', ['copy:html','useminPrepare','concat', 'uglify', 'cssmin', 'htmlmin', 'imagemin','clean:end']);
+  grunt.registerTask('build', ['copy:html', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'htmlmin', 'imagemin', 'clean:end']);
 };
-;
-
